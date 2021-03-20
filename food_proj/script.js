590571d1eb89b259d47f000043f8a50b79eef7ed
Svyatoslav <SvyatoslavS13@gmail.com>
2021-03-20 21:02:18 +0300

    first section

---
+++ food_proj/script.js
@@ -0,0 +1,17 @@
+const icons = document.querySelectorAll('.section-1-icons i');
+
+let counter = 1;
+
+setInterval(() => {
+   counter++;
+   const icon = document.querySelector('.section-1-icons .change');
+   icon.classList.remove('change');
+
+   if(counter > icons.length){
+   icons[0].classList.add('change');
+   counter = 1;
+   } else {
+      icon.nextElementSibling.classList.add('change');
+   }
+
+}, 1000);
