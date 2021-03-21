ec8fdcd6b1f724b71ef33b1bc064c2c2297a247e
Svyatoslav <SvyatoslavS13@gmail.com>
2021-03-21 22:54:04 +0300

    completed  navbar

--- food_proj/script.js
+++ food_proj/script.js
@@ -1,3 +1,4 @@
+
 const icons = document.querySelectorAll('.section-1-icons i');
 
 let counter = 1;
@@ -15,3 +16,9 @@ setInterval(() => {
    }
 
 }, 4000);
+
+document.querySelector('.menu').addEventListener('click', () => {
+	document.querySelectorAll('.target').forEach((item) => {
+		item.classList.toggle('change');
+	})
+})
